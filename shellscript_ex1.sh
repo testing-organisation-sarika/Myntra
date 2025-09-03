8ec081799d0922c9c341bc137e18a99f6bb7ea2d
devopsu <90164902+devopsu@users.noreply.github.com>
2025-09-03 23:04:46 +0530

    Create shellscript_ex1.sh

---
+++ shellscript_ex1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# script to print name and date 
+
+echo "Hii! My Name Is Harshitha,I'm Learning Linux And Shell Scripting"
+
+echo "Today is:$(date)"
+
+# script to  print system uptime and the logged-in-user 
+
+echo "Logged in as:$(whoami)"
+
+echo "System uptime:$(uptime)"
