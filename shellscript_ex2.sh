00038090be4dacc995915432297429e0261b1b86
devopsu <90164902+devopsu@users.noreply.github.com>
2025-09-03 23:05:46 +0530

    Create shellscript_ex2.sh

---
+++ shellscript_ex2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Ask the user name and read the name 
+
+echo "Enter your name "
+read name 
+
+# Ask the user for their fav devops tool
+
+echo "Enter your fav devops tool"
+read tool
+
+# Output 
+
+echo "Hii!${name}"
+
+echo "Your fav devops tool:${tool}"
